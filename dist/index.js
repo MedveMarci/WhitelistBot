27db8a4934ac7b2816c037538cdc5dfd06a9744c
MedveMarci <medvemarci4@gmail.com>
2024-06-25 21:45:31 +0200

    V1.0 out

--- dist/index.js
+++ dist/index.js
@@ -25,7 +25,7 @@ client.on(discord_js_1.Events.ClientReady, () => __awaiter(void 0, void 0, void 
             client.guilds.cache.first().name));
     }
     try {
-        WhitelistInitialize();
+        yield whitelistInitialize();
         console.log(chalk_1.default.green(`${(_a = client.user) === null || _a === void 0 ? void 0 : _a.username} sikeresen elindult!`));
     }
     catch (e) {
@@ -37,9 +37,9 @@ client.login(config_json_1.default.Token).then().catch(e => {
     console.error(chalk_1.default.red("Hiba történt a bejelentkezés során!"));
     console.log(e);
 });
-function WhitelistInitialize() {
-    var _a;
+function whitelistInitialize() {
     return __awaiter(this, void 0, void 0, function* () {
+        var _a;
         const role = (_a = client.guilds.cache.first()) === null || _a === void 0 ? void 0 : _a.roles.cache.get(config_json_1.default.WhitelistRoleID);
         if (config_json_1.default.WhitelistChannelID === "") {
             console.error(chalk_1.default.red("A Whitelist Channel ID nem lehet üres!"));
@@ -53,7 +53,7 @@ function WhitelistInitialize() {
             console.error(chalk_1.default.red("A rang nem található!"));
             process.exit(1);
         }
-        const channel = CheckChannel();
+        const channel = checkChannel();
         const embed = new discord_js_1.EmbedBuilder()
             .setTitle(`Whitelist rendszer`)
             .setDescription("Kattints a gombra hogy elindítsd a Whitelist tesztet!")
@@ -80,7 +80,7 @@ function WhitelistInitialize() {
                     console.log(`Hiba a file írásakor `, e);
                 }
             }
-            if (lastMessage.author.id != ((_b = client.user) === null || _b === void 0 ? void 0 : _b.id)) {
+            if (lastMessage.author.id !== ((_b = client.user) === null || _b === void 0 ? void 0 : _b.id)) {
                 const message = yield (channel === null || channel === void 0 ? void 0 : channel.send({ embeds: [embed], components: [row] }));
                 config_json_1.default.WhitelistMessageID = message === null || message === void 0 ? void 0 : message.id;
                 try {
@@ -99,45 +99,132 @@ function WhitelistInitialize() {
                 const member = (_c = client.guilds.cache.first()) === null || _c === void 0 ? void 0 : _c.members.cache.get(interaction.user.id);
                 if (member == null)
                     return;
-                if (doing.includes(member.id))
-                    yield interaction.reply({ content: "Már elkezdtél egy tesztet!", ephemeral: true });
-                if (interaction.replied)
-                    return;
                 if (member.roles.cache.has(role.id))
-                    yield interaction.reply({ content: "Már rajta vagy a Whitelisten!", ephemeral: true });
+                    yield interaction.reply({
+                        content: "Már rajta vagy a Whitelisten!",
+                        ephemeral: true
+                    });
                 if (interaction.replied)
                     return;
+                if (doing.find(item => item.id === member.id))
+                    return interaction.reply({
+                        content: "Már elindítottad a tesztet!",
+                        ephemeral: true
+                    });
+                if (interaction.replied)
+                    return;
+                doing.push({
+                    id: member.id,
+                    wrong: 0,
+                    question: 0,
+                    completedQuestions: []
+                });
                 const embed = new discord_js_1.EmbedBuilder()
                     .setTitle(`Whitelist teszt`)
-                    .setDescription("Bizonyos számú kérdésre kell válaszolnod, hogy megkapd a Whitelist rangot! 3 hibázási lehetőséged van!")
+                    .setDescription(`Bizonyos számú kérdésre kell válaszolnod, hogy megkapd a Whitelist rangot! ${config_json_1.default.MaxWrong} hibázási lehetőséged van!`)
                     .setColor("Green")
                     .setThumbnail(client.guilds.cache.first().iconURL())
                     .setFooter({ text: "A botot MedveMarci készítette" });
-                yield member.send({ embeds: [embed] }).catch(() => interaction.reply({ content: "Nem tudtam elküldeni a privát üzenetet!", ephemeral: true }));
+                yield member.send({ embeds: [embed] }).catch(() => {
+                    doing = doing.filter(item => item.id !== member.id);
+                    interaction.reply({
+                        content: "Nem tudtam elküldeni a privát üzenetet!",
+                        ephemeral: true
+                    });
+                });
                 if (interaction.replied)
                     return;
-                doing.push(member.id);
                 interaction.reply({ content: "Sikeresen elindítottad a tesztet!", ephemeral: true });
-                StartDMQuestion(member);
+                yield nextQuestion(member);
+            }
+        }));
+        client.on(discord_js_1.Events.InteractionCreate, (interaction) => __awaiter(this, void 0, void 0, function* () {
+            var _d, _e, _f;
+            if (!interaction.isStringSelectMenu())
+                return;
+            if (interaction.customId.split("-")[0] !== "question")
+                return;
+            const member = (_d = client.guilds.cache.first()) === null || _d === void 0 ? void 0 : _d.members.cache.get(interaction.user.id);
+            const correct = interaction.customId.split("-")[1];
+            const answer = Number(interaction.values[0]) - 1;
+            if (member == null)
+                return;
+            if (answer === Number(correct)) {
+                yield interaction.reply({ content: "Helyes válasz!", ephemeral: true });
+                const data = doing.find(item => item.id === member.id);
+                if (data == null)
+                    return;
+                data.completedQuestions.push(data.question);
+                if (data.completedQuestions.length >= Object.keys(config_json_1.default.Test).length) {
+                    const role = (_e = client.guilds.cache.first()) === null || _e === void 0 ? void 0 : _e.roles.cache.get(config_json_1.default.WhitelistRoleID);
+                    if (role == null)
+                        return;
+                    yield member.roles.add(role).catch((e) => {
+                        console.error(chalk_1.default.red("Hiba történt a rang hozzáadásakor!"), e);
+                    });
+                    yield interaction.editReply({ content: "Helyes válasz!\nSikeresen teljesítetted a tesztet, megkaptad a Whitelist rangot!" }).catch(() => console.error(chalk_1.default.red("Nem tudtam elküldeni a privát üzenetet!")));
+                    doing = doing.filter(item => item.id !== member.id);
+                    return;
+                }
+                else
+                    yield nextQuestion(member);
+            }
+            else {
+                yield interaction.reply({ content: "Hibás válasz!", ephemeral: true });
+                const data = doing.find(item => item.id === member.id);
+                if (data == null)
+                    return;
+                data.completedQuestions.push(data.question);
+                data.wrong++;
+                if (data.wrong >= config_json_1.default.MaxWrong) {
+                    yield interaction.editReply({ content: "Hibás válasz!\nSajnos nem sikerült a teszt, próbáld újra később!" }).catch(() => console.error(chalk_1.default.red("Nem tudtam elküldeni a privát üzenetet!")));
+                    doing = doing.filter(item => item.id !== member.id);
+                    return;
+                }
+                else if (data.completedQuestions.length >= Object.keys(config_json_1.default.Test).length) {
+                    const role = (_f = client.guilds.cache.first()) === null || _f === void 0 ? void 0 : _f.roles.cache.get(config_json_1.default.WhitelistRoleID);
+                    if (role == null)
+                        return;
+                    yield member.roles.add(role).catch((e) => {
+                        console.error(chalk_1.default.red("Hiba történt a rang hozzáadásakor!"), e);
+                    });
+                    yield interaction.editReply({ content: "Hibás válasz!\nSikeresen teljesítetted a tesztet, megkaptad a Whitelist rangot!" }).catch(() => console.error(chalk_1.default.red("Nem tudtam elküldeni a privát üzenetet!")));
+                    doing = doing.filter(item => item.id !== member.id);
+                    return;
+                }
+                else
+                    yield nextQuestion(member);
             }
         }));
     });
 }
-function StartDMQuestion(member) {
-    if (member == null)
-        return;
-    for (let item of config_json_1.default.Test) {
-        let test = item;
+function nextQuestion(member) {
+    return __awaiter(this, void 0, void 0, function* () {
+        var _a, _b;
+        if (member == null)
+            return;
+        const data = doing.find(item => item.id === member.id);
+        if (data == null)
+            return;
+        if (config_json_1.default.RandomOrder) {
+            const random = Math.floor(Math.random() * Object.keys(config_json_1.default.Test).length);
+            if (data.completedQuestions.includes(random)) {
+                return nextQuestion(member);
+            }
+            data.question = random;
+        }
+        const q = data.question;
         let question = "";
         let options = [];
         let correct = 0;
-        for (let key in test) {
-            let value = test[key];
-            if (key != "Megoldás" && typeof value === "object") {
+        for (const key in config_json_1.default.Test[q]) {
+            const test = config_json_1.default.Test[q];
+            const value = test[key];
+            if (key !== "Megoldás" && typeof value === "object") {
                 question = key;
                 options = value;
             }
-            if (key == "Megoldás") {
+            if (key === "Megoldás") {
                 correct = value;
             }
         }
@@ -147,18 +234,42 @@ function StartDMQuestion(member) {
             .setThumbnail(client.guilds.cache.first().iconURL())
             .setFooter({ text: "A botot MedveMarci készítette" });
         for (let j = 0; j < options.length; j++) {
-            embed.addFields({ name: `${j + 1}.`, value: options[j], inline: true });
+            embed.addFields({ name: options[j], value: "\u200B", inline: false });
         }
-        member.send({ embeds: [embed] }).catch(() => member.send({ content: "Nem tudtam elküldeni a kérdést!" }));
-    }
+        const select = new discord_js_1.StringSelectMenuBuilder()
+            .setCustomId(`question-${correct - 1}`)
+            .setPlaceholder('1 válaszra nyomj rá!');
+        for (let i = 0; i < options.length; i++) {
+            const option = new discord_js_1.StringSelectMenuOptionBuilder()
+                .setLabel(`${i + 1}.`)
+                .setValue(`${i + 1}`)
+                .setDescription(options[i]);
+            select.addOptions(option);
+        }
+        const row = new discord_js_1.ActionRowBuilder()
+            .addComponents(select);
+        if (data.completedQuestions.length === 0) {
+            yield member.send({ embeds: [embed], components: [row] }).catch(() => {
+                doing = doing.filter(item => item.id !== member.id);
+                member.send({ content: "Nem tudtam elküldeni a kérdést!" });
+            });
+        }
+        else {
+            yield ((_b = (_a = member.dmChannel) === null || _a === void 0 ? void 0 : _a.messages.cache.last()) === null || _b === void 0 ? void 0 : _b.edit({ embeds: [embed], components: [row] }).catch(() => {
+                doing = doing.filter(item => item.id !== member.id);
+                member.send({ content: "Nem tudtam szerkeszteni a kérdést!" });
+            }));
+        }
+        data.question++;
+    });
 }
-function GetChannel(id) {
+function getChannel(id) {
     var _a;
     const channel = (_a = client.guilds.cache.first()) === null || _a === void 0 ? void 0 : _a.channels.cache.get(`${id}`);
     return channel;
 }
-function CheckChannel() {
-    const channel = GetChannel(config_json_1.default.WhitelistChannelID);
+function checkChannel() {
+    const channel = getChannel(config_json_1.default.WhitelistChannelID);
     if (channel == null) {
         console.error(chalk_1.default.red("A csatorna nem található!"));
         return;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, EmbedBuilder, Events, GatewayIntentBits, GuildMember, TextChannel } from "discord.js";
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    Client,
+    EmbedBuilder,
+    Events,
+    GatewayIntentBits,
+    GuildMember, StringSelectMenuBuilder, StringSelectMenuOptionBuilder,
+    TextChannel
+} from "discord.js";
 import chalk from "chalk";
 import config from "../config.json";
 import fs from "fs";
 
-const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences ] });
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences]});
 let doing: any[] = [];
 
 client.on(Events.ClientReady, async () => {
@@ -14,7 +24,7 @@ client.on(Events.ClientReady, async () => {
         ));
     }
     try {
-        WhitelistInitialize();
+        await whitelistInitialize();
         console.log(chalk.green(`${client.user?.username} sikeresen elindult!`));
     } catch (e) {
         console.log(e);
@@ -27,7 +37,7 @@ client.login(config.Token).then().catch(e => {
     console.log(e);
 });
 
-async function WhitelistInitialize() {
+async function whitelistInitialize() {
     const role = client.guilds.cache.first()?.roles.cache.get(config.WhitelistRoleID);
     if (config.WhitelistChannelID === "") {
         console.error(chalk.red("A Whitelist Channel ID nem lehet üres!"));
@@ -41,23 +51,23 @@ async function WhitelistInitialize() {
         console.error(chalk.red("A rang nem található!"));
         process.exit(1);
     }
-    const channel = CheckChannel();
+    const channel = checkChannel();
     const embed = new EmbedBuilder()
-    .setTitle(`Whitelist rendszer`)
-    .setDescription("Kattints a gombra hogy elindítsd a Whitelist tesztet!")
-    .setColor("Green")
-    .setThumbnail(client.guilds.cache.first()!.iconURL()!)
-    .setFooter({ text: "A botot MedveMarci készítette" });
+        .setTitle(`Whitelist rendszer`)
+        .setDescription("Kattints a gombra hogy elindítsd a Whitelist tesztet!")
+        .setColor("Green")
+        .setThumbnail(client.guilds.cache.first()!.iconURL()!)
+        .setFooter({text: "A botot MedveMarci készítette"});
     const row = new ActionRowBuilder<ButtonBuilder>();
     const button = new ButtonBuilder()
         .setStyle(ButtonStyle.Success)
         .setLabel("Whitelist teszt")
         .setCustomId("whitelist_test");
     row.addComponents(button);
-    channel?.messages.fetch({ limit: 1 }).then(async (messages: { first: () => any; }) => {
+    channel?.messages.fetch({limit: 1}).then(async (messages: { first: () => any; }) => {
         let lastMessage = messages.first();
         if (lastMessage == null) {
-            const message = await channel?.send({ embeds: [ embed ], components: [ row ]});
+            const message = await channel?.send({embeds: [embed], components: [row]});
             lastMessage = message;
             config.WhitelistMessageID = message?.id;
             try {
@@ -66,8 +76,8 @@ async function WhitelistInitialize() {
                 console.log(`Hiba a file írásakor `, e);
             }
         }
-        if (lastMessage.author.id != client.user?.id) {
-            const message = await channel?.send({ embeds: [ embed ], components: [ row ] });
+        if (lastMessage.author.id !== client.user?.id) {
+            const message = await channel?.send({embeds: [embed], components: [row]});
             config.WhitelistMessageID = message?.id;
             try {
                 fs.writeFileSync(`./config.json`, JSON.stringify(config, null, 2));
@@ -81,63 +91,156 @@ async function WhitelistInitialize() {
         if (interaction.customId === "whitelist_test") {
             const member = client.guilds.cache.first()?.members.cache.get(interaction.user.id);
             if (member == null) return;
-            if (member.roles.cache.has(role.id)) await interaction.reply({ content: "Már rajta vagy a Whitelisten!", ephemeral: true });
+            if (member.roles.cache.has(role.id)) await interaction.reply({
+                content: "Már rajta vagy a Whitelisten!",
+                ephemeral: true
+            });
             if (interaction.replied) return;
-            if (doing.includes(member.id)) await interaction.reply({ content: "Már elkezdtél egy tesztet!", ephemeral: true });
+            if (doing.find(item => item.id === member.id)) return interaction.reply({
+                content: "Már elindítottad a tesztet!",
+                ephemeral: true
+            });
             if (interaction.replied) return;
+            doing.push({
+                id: member.id,
+                wrong: 0,
+                question: 0,
+                completedQuestions: []
+            });
             const embed = new EmbedBuilder()
-            .setTitle(`Whitelist teszt`)
-            .setDescription("Bizonyos számú kérdésre kell válaszolnod, hogy megkapd a Whitelist rangot! 3 hibázási lehetőséged van!")
-            .setColor("Green")
-            .setThumbnail(client.guilds.cache.first()!.iconURL()!)
-            .setFooter({ text: "A botot MedveMarci készítette" });
-            await member.send({embeds: [embed]}).catch(() => interaction.reply({ content: "Nem tudtam elküldeni a privát üzenetet!", ephemeral: true }));
+                .setTitle(`Whitelist teszt`)
+                .setDescription(`Bizonyos számú kérdésre kell válaszolnod, hogy megkapd a Whitelist rangot! ${config.MaxWrong} hibázási lehetőséged van!`)
+                .setColor("Green")
+                .setThumbnail(client.guilds.cache.first()!.iconURL()!)
+                .setFooter({text: "A botot MedveMarci készítette"});
+            await member.send({embeds: [embed]}).catch(() => {
+                    doing = doing.filter(item => item.id !== member.id);
+                    interaction.reply({
+                        content: "Nem tudtam elküldeni a privát üzenetet!",
+                        ephemeral: true
+                    });
+                }
+            );
             if (interaction.replied) return;
-            doing.push(member.id);
-            interaction.reply({ content: "Sikeresen elindítottad a tesztet!", ephemeral: true });
-            StartDMQuestion(member);
-    }});
-}
-
-function StartDMQuestion(member: GuildMember) {
-    if (member == null) return;
-    for (let item of config.Test) {
-        let test: { [key: string]: number | string[] | undefined } = item;
-        let question = ""
-        let options: string | any[] = []
-        let correct = 0;
-        for (let key in test) {
-            let value = test[key];
-            if (key != "Megoldás" && typeof value === "object") {
-                question = key;
-                options = value as string[];
-            }
-            if (key == "Megoldás") {
-                correct = value as number;
-            }
+            interaction.reply({content: "Sikeresen elindítottad a tesztet!", ephemeral: true});
+            await nextQuestion(member);
         }
-        const embed = new EmbedBuilder()
-            .setTitle(question)
-            .setColor("Blue")
-            .setThumbnail(client.guilds.cache.first()!.iconURL()!)
-            .setFooter({ text: "A botot MedveMarci készítette" });
-            for (let j = 0; j < options.length; j++) {
-                embed.addFields({name: `${j + 1}.`, value: options[j], inline: true});
-            }
-        member.send({ embeds: [embed] }).catch(() => member.send({ content: "Nem tudtam elküldeni a kérdést!" }));
-    }
+    });
+    client.on(Events.InteractionCreate, async interaction => {
+        if (!interaction.isStringSelectMenu()) return;
+        if (interaction.customId.split("-")[0] !== "question") return;
+        const member = client.guilds.cache.first()?.members.cache.get(interaction.user.id);
+        const correct = interaction.customId.split("-")[1];
+        const answer = Number(interaction.values[0]) - 1;
+        if (member == null) return;
+        if (answer === Number(correct)) {
+            await interaction.reply({content: "Helyes válasz!", ephemeral: true});
+            const data = doing.find(item => item.id === member.id);
+            if (data == null) return;
+            data.completedQuestions.push(data.question);
+            if (data.completedQuestions.length >= Object.keys(config.Test).length) {
+                const role = client.guilds.cache.first()?.roles.cache.get(config.WhitelistRoleID);
+                if (role == null) return;
+                await member.roles.add(role).catch((e: any) => {
+                    console.error(chalk.red("Hiba történt a rang hozzáadásakor!"), e);
+                });
+                await interaction.editReply({content: "Helyes válasz!\nSikeresen teljesítetted a tesztet, megkaptad a Whitelist rangot!"}).catch(() => console.error(chalk.red("Nem tudtam elküldeni a privát üzenetet!")));
+                doing = doing.filter(item => item.id !== member.id);
+                return;
+            } else await nextQuestion(member);
+        } else {
+            await interaction.reply({content: "Hibás válasz!", ephemeral: true});
+            const data = doing.find(item => item.id === member.id);
+            if (data == null) return;
+            data.completedQuestions.push(data.question);
+            data.wrong++;
+            if (data.wrong >= config.MaxWrong) {
+                await interaction.editReply({content: "Hibás válasz!\nSajnos nem sikerült a teszt, próbáld újra később!"}).catch(() => console.error(chalk.red("Nem tudtam elküldeni a privát üzenetet!")));
+                doing = doing.filter(item => item.id !== member.id);
+                return;
+            } else if (data.completedQuestions.length >= Object.keys(config.Test).length) {
+                const role = client.guilds.cache.first()?.roles.cache.get(config.WhitelistRoleID);
+                if (role == null) return;
+                await member.roles.add(role).catch((e: any) => {
+                    console.error(chalk.red("Hiba történt a rang hozzáadásakor!"), e);
+                });
+                await interaction.editReply({content: "Hibás válasz!\nSikeresen teljesítetted a tesztet, megkaptad a Whitelist rangot!"}).catch(() => console.error(chalk.red("Nem tudtam elküldeni a privát üzenetet!")));
+                doing = doing.filter(item => item.id !== member.id);
+                return;
+            } else await nextQuestion(member);
+        }
+    });
 }
 
-/*member.roles.add(role).catch((e: any) => {
-                console.error(chalk.red("Hiba történt a rang hozzáadásakor!"), e);
-            });*/
-function GetChannel(id: string) {
+async function nextQuestion(member: GuildMember) {
+    if (member == null) return;
+    const data = doing.find(item => item.id === member.id);
+    if (data == null) return;
+    if (config.RandomOrder) {
+        const random = Math.floor(Math.random() * Object.keys(config.Test).length);
+        if (data.completedQuestions.includes(random)) {
+            return nextQuestion(member);
+        }
+        data.question = random;
+    }
+    const q = data.question;
+    let question = "";
+    let options: string | any[] = [];
+    let correct = 0;
+    for (const key in config.Test[q]) {
+        const test: { [key: string]: number | string[] | undefined } = config.Test[q];
+        const value = test[key];
+        if (key !== "Megoldás" && typeof value === "object") {
+            question = key;
+            options = value as string[];
+        }
+        if (key === "Megoldás") {
+            correct = value as number;
+        }
+    }
+    const embed = new EmbedBuilder()
+        .setTitle(question)
+        .setColor("Blue")
+        .setThumbnail(client.guilds.cache.first()!.iconURL()!)
+        .setFooter({text: "A botot MedveMarci készítette"});
+    for (let j = 0; j < options.length; j++) {
+        embed.addFields({name: options[j], value: "\u200B", inline: false});
+    }
+    const select = new StringSelectMenuBuilder()
+        .setCustomId(`question-${correct - 1}`)
+        .setPlaceholder('1 válaszra nyomj rá!');
+    for (let i = 0; i < options.length; i++) {
+        const option = new StringSelectMenuOptionBuilder()
+            .setLabel(`${i + 1}.`)
+            .setValue(`${i + 1}`)
+            .setDescription(options[i]);
+        select.addOptions(option);
+    }
+    const row = new ActionRowBuilder()
+        .addComponents(select);
+    if (data.completedQuestions.length === 0) {
+        // @ts-ignore
+        await member.send({embeds: [embed], components: [row]}).catch(() => {
+            doing = doing.filter(item => item.id !== member.id);
+            member.send({content: "Nem tudtam elküldeni a kérdést!"});
+        });
+    } else {
+        // @ts-ignore
+        await member.dmChannel?.messages.cache.last()?.edit({embeds: [embed], components: [row]}).catch(() => {
+            doing = doing.filter(item => item.id !== member.id);
+            member.send({content: "Nem tudtam szerkeszteni a kérdést!"});
+        });
+    }
+    data.question++;
+}
+
+function getChannel(id: string) {
     const channel = client.guilds.cache.first()?.channels.cache.get(`${id}`);
     return channel as TextChannel;
 }
 
-function CheckChannel() {
-    const channel = GetChannel(config.WhitelistChannelID!);
+function checkChannel() {
+    const channel = getChannel(config.WhitelistChannelID!);
     if (channel == null) {
         console.error(chalk.red("A csatorna nem található!"));
         return;
